3cda64fdf5f3493cf298fff9ddd07222d9e32344
JohnSV24 <jasv0124@gmail.com>
2024-07-24 17:45:00 -0600

    add lines of slice_sample for the muestra_aleatoria

--- Muestreo-aleatorio-simple.R
+++ Muestreo-aleatorio-simple.R
@@ -6,7 +6,16 @@ library(sampling)
 #dplyr especializado en data frames y analisis de datos
 library(dplyr)
 
-#set.seed(numero-para-aleatorizar)
-set.seed(1)
-base
 #Consejo: Puedes hacer ctrl + enter en una linea para correr la linea
+#base es el import DataSet
+base
+#set.seed(numero-para-aleatorizar)
+set.seed(109)
+#set.seed() Al establecer una semilla, te aseguras de que las operaciones que impliquen aleatoriedad produzcan los mismos resultados cada vez que se ejecuten con la misma semilla.
+
+
+#Muestra aleatoria
+#slice_sample(.data = de-donde-obtiene-los-datos, n = num-muestra, replace = si hay reemplazo o no)
+muestra_aleatoria <- slice_sample(.data = base, n = 8, replace = FALSE)
+#llamado a muestra aleatoria
+muestra_aleatoria

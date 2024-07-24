1144418be2b33ac5e48b8e41263d842fe34fa614
JohnSV24 <jasv0124@gmail.com>
2024-07-24 17:23:25 -0600

    commit inicial de ejercicio de muestreo aleatorio simple

---
+++ Muestreo-aleatorio-simple.R
@@ -0,0 +1,7 @@
+#Muestreo Aleatorio Simple
+# install.packages("DescTools") <- para instalar paquetes
+#library(DescTools) <- para poner libreria, ejemplo estadistica descriptiva
+#sampling especializado en muestreo 
+library(sampling)
+#dplyr especializado en data frames y analisis de datos
+library(dplyr)

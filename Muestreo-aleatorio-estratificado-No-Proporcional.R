76537c401a2229d0242c11fadfdac1fad38e767d
JohnSV24 <jasv0124@gmail.com>
2024-07-25 12:22:22 -0600

    add data.frame line to know the estratos of the Dataset

--- Muestreo-aleatorio-estratificado-No-Proporcional.R
+++ Muestreo-aleatorio-estratificado-No-Proporcional.R
@@ -20,6 +20,14 @@ set.seed(10)
 muestra_aleatoria <- slice_sample(.data = base, n = 8, replace = F)
 muestra_aleatoria
 
+#Como saber cuantos estratos tiene mi dataFrame
+data.frame(table(base$nivel_estudios))
+#data.frame(table(Dataset$col-name)): nos ayudara a saber el num son los estratos
+#Ejemplo de: data.frame(table(base$nivel_estudios))
+#   Var1      Freq
+#1  Basico    22
+#2M.Superior  24
+#3Superior    24
 #Muestreo Aleatorio estratificado no proporcional
 set.seed(10)
 muestra_est_np <- strata(data = base, stratanames = c("nivel_estudios"), size = c(3,5,4), method = "srswor")

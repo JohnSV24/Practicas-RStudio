eeadb74efe48451807ef7d7418241268fb6755ac
JohnSV24 <jasv0124@gmail.com>
2024-07-25 14:15:16 -0600

    add lines for cal of n_superior, n_basico, n_medio_superior

--- Muestreo-Aleatorio-Estratificado-Proporcional.R
+++ Muestreo-Aleatorio-Estratificado-Proporcional.R
@@ -1,3 +1,18 @@
 #Muestreo aleatorio Estratificado Proporcional
 library(sampling)
 library(dplyr)
+
+#Obtener los estratos del DataSet y nos indica cuantos tiene cada uno
+data.frame(table(base$nivel_estudios))
+
+#Nivel superior
+n_superior <- sum(with(base, nivel_estudios == "Superior"))/nrow(base)
+#n_superior <- 24/70
+#nrow(base) = 70
+#sum(with(base, nivel_estudios == "Superior")) = 24
+#La funcion: sum(with(base, nivel_estudios == "Superior")) indica cuantos TRUE
+#de nuestro Dataset hay del nivel_estuidos con "Superior"
+
+n_basico <- sum(with(base, nivel_estudios == "Básico"))/nrow(base)
+n_medio_superior <- sum(with(base, nivel_estudios == "Medio Superior"))/nrow(base)
+
